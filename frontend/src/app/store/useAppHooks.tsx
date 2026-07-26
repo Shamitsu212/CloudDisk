c1de0cad24ab21c234ba5f9364ea744cc4ba0521
Shamitsu212 <darklin314@gmail.com>
2026-07-26 22:28:07 +0300

    chore: Удалена папка backend

--- frontend/src/app/store/useAppHooks.tsx
+++
@@ -1,9 +0,0 @@
-import { useDispatch } from "react-redux";
-import type { AppDispatch } from "./store";
-
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-
-import { type TypedUseSelectorHook, useSelector } from "react-redux";
-import type { RootState } from "./store";
-
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
