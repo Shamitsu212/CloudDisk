5d867fdbef2e071f1e08d7a3446113d07598ccc4
Shamitsu212 <darklin314@gmail.com>
2026-07-28 12:14:49 +0300

    fix: Удалена тестовая папка из initialState

--- frontend/src/main/slice/folderSlice.ts
+++ frontend/src/main/slice/folderSlice.ts
@@ -5,18 +5,7 @@ import type { FolderState } from "./types";
 import { fetchFolders, createFolder, deleteFolder, renameFolder, favoriteFolder } from "./folderThunks";
 
 const initialState: FolderState = {
-    folders: [
-        {
-            id: 1,
-            user_id: 2,
-
-            name: "Доки",
-            files: 12,
-
-            lastUpdate: "",
-            isFavorite: false,
-        }
-    ],
+    folders: [],
     loading: false,
     error: null,
 };
